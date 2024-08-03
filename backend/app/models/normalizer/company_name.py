49b361edcc5427fa32ea8c3a0c396db448f5f37f
yuto-kimura-g <75498217+yuto-kimura-g@users.noreply.github.com>
2024-08-03 14:34:33 +0900

    [update] 全角英数字 -> 半角英数字

--- backend/app/models/normalizer/company_name.py
+++ backend/app/models/normalizer/company_name.py
@@ -19,4 +19,13 @@ class CompanyNameNormalizer:
                 >>> CompanyNameNormalizer().normalize("  株式会社テスト  ")
                 "株式会社テスト"
         """
-        return company_name.strip()
+        # return company_name.strip()
+        # 左右の空白を除去
+        company_name = company_name.strip()
+
+        # 全角英数字を半角英数字に揃える
+        # 謎のライブラリに丸投げだけどまあいいでしょう．
+        import unicodedata
+        company_name = unicodedata.normalize("NFKC", company_name)
+
+        return company_name
