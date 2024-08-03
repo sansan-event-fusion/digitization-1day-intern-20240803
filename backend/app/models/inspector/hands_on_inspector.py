e13d7bf5bfabcd211620ce37bdfdb2ded754ed25
yuto-kimura-g <75498217+yuto-kimura-g@users.noreply.github.com>
2024-08-03 16:37:00 +0900

    [feat] inspector: 短縮系，敬称，日英併記

--- backend/app/models/inspector/hands_on_inspector.py
+++ backend/app/models/inspector/hands_on_inspector.py
@@ -30,6 +30,27 @@ class Inspector(BaseInspector):
         # if re.search(r"\.c0m$", model.entry.email):
         #     result.inspect(EntryItems.email)
 
+        # [work] （株）（有）の検知
+        short_types = ["（株）", "（有）"]
+        for short_type in short_types:
+            if short_type in model.entry.company_name:
+                result.inspect(EntryItems.company_name)
+
+        # [work] Mr. Dr. の検知
+        titles = ["Mr.", "Dr."]
+        for title in titles:
+            if title in model.entry.full_name:
+                result.inspect(EntryItems.full_name)
+
+        # [work] 日英併記の検知
+        # 氏名，会社名，部署名，住所 それぞれを確認する
+        target_props = ["full_name", "company_name", "position_name", "address"]
+        for prop in target_props:
+            data = model.entry.dict().get(prop)
+            is_combined: bool = len(set([unicodedata.east_asian_width(char) in "WH" for char in data])) > 1
+            if is_combined:
+                result.inspect(eval(f"EntryItems.{prop}"))
+
         # DEBUG:
         # print("# Ln:inspect()")
 
