14f78b77b157fe2882d6acf807d0fecc27fe9a4e
yoshiyuki-140 <c1221251@st.kanazawa-it.ac.jp>
2024-08-03 15:43:25 +0900

    update : インスペクターのハンズオン完了

--- backend/app/models/inspector/hands_on_inspector.py
+++ backend/app/models/inspector/hands_on_inspector.py
@@ -1,4 +1,5 @@
 import unicodedata
+import re
 
 from app.models.entry import EntryItems
 from app.models.inspector.base import BaseInspector, InspectedVirtualCardModel
@@ -12,11 +13,15 @@ class Inspector(BaseInspector):
 
         # 条件に合致したら EntryItems.company_name を渡して実行
         # inspected_items を更新してくれる
+        ## 会社名に全角英数字が含まれている場合に会社名（company_name）
+        ## をリセットする場合だと以下䛾ような実装になります
         for char in model.entry.company_name:
             if unicodedata.east_asian_width(char) == "F":
                 result.inspect(EntryItems.company_name)
                 break
 
         # TODO: ここにロジックを追加する
+        if re.search(r"\.c0m$", model.entry.email):
+            result.inspect(EntryItems.email)
 
         return result
